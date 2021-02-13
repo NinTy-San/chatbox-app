4f83b1ae78c22e5ad3e9f46c9c723022a7474203
NineTy-San <40755650+NinTy-San@users.noreply.github.com>
2021-02-13 21:35:33 +0100

    Message
    - createMessage
    - addMessage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import Formulaire from './components/Fomulaire'
 import Message from './components/Message'
 
 class App extends Component {
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage =  message => {
+    const messages = { ... this.state.messages} // on copie le state
+    messages[`message-${Date.now()}`] = message // on modifie le  state avec un timestamp pour rendre le message unique
+    this.setState({ messages }) // on met à jour le message
+  }
   render () {
     return (
       <div className='box'>
         <div>
           <div className="message">
             <Message></Message>
+            <Message></Message>
+            <Message></Message>
           </div>
         </div>
-        <Formulaire />
+        <Formulaire 
+         pseudo={this.state.pseudo}
+         addMessage={this.addMessage} />
       </div>
     )
   }
